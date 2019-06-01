824f635ba16e1dc616dc631fcce884e062202eb0
Andrew Busch <andrew.busch7@gmail.com>
2019-06-01 11:33:45 +1000

    Can get list of commits since hash, working on getting diff

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,19 @@ const originalHash = 'c15c91c531bc0069b249e737de8fb81b6aeadf60';
 const git = simplegit(path);
 // git.status().then((status: StatusResult) => console.log(status));
 
-git.log({
-	n: 20
-}).then((value: ListLogSummary<DefaultLogFields>) => console.log(value.all));
+const commits: string[] = [];
+let foundStart = false;
+let skip = 0;
 
-// git.diff(['0a369065286c03ed03cbe4e67b40921bdd18d5ed^!']).then((value: string) => console.log(value));
-// git.diffSummary(['0a369065286c03ed03cbe4e67b40921bdd18d5ed^!']).then(value => console.log(value));
+git
+	.diff(['c15c91c531bc0069b249e737de8fb81b6aeadf60~', 'c15c91c531bc0069b249e737de8fb81b6aeadf60'])
+	.then(e => console.log(e));
 
-// const promptForPath = () => {
-//   npmPrompt.start();
-//   npmPrompt.get(['path'], (err: any, result: any) =>
-//     readDirectory(result.path)
-//   );
-// };
 
-// const readDirectory = (path: string) =>
-//   readdir(path, (err: NodeJS.ErrnoException, files: string[]) => {
-//     files.forEach(file => {
-//       console.log(file);
-//     });
-//   });
+// git.log({
+// 	from: originalHash,
+// 	to: 'HEAD'
+// }).then((value: ListLogSummary<DefaultLogFields>) => {
+// 	const commits = value.all;
+// 	console.log(commits);
+// });
